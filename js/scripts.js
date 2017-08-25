f2e31423146925cf879ed626c292b0df3f89e957
Mike Holland <michaellholland@hotmail.com>
2017-08-25 14:19:46 -0700

    add alert for total, troubleshoot prototype and constructors

--- js/scripts.js
+++ js/scripts.js
@@ -4,25 +4,20 @@ function Pizza(size, toppings) {
 
 }
 
+Ticket.prototype.pizzaTotal = function () {
+  return this.size + this.toppings
 
-
-
-
-
-
-
-
-
-
+}
 
 
 $(document).ready(function() {
   $(#"pizza").submit(function(event) {
-
+    event.preventDefault();
 
     var sizeInput = $("#size").val();
     var toppingInput = $("#toppings").val();
 
-    var
-  }
-}
+    var NewPizza = new Pizza(parseInt(sizeInput), parseInt(toppingInput));
+    alert(newPizza.pizzaTotal());
+  });
+});
