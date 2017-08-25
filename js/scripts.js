5c65a9038303d0cc231998fc1bc84e3c4a4a1701
Mike Holland <michaellholland@hotmail.com>
2017-08-25 13:31:25 -0700

    begin pizza constructor and pricing prototype

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+function Pizza(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+  $(#"pizza").submit(function(event) {
+
+
+    var sizeInput = $("#size").val();
+    var toppingInput = $("#toppings").val();
+
+    var
+  }
+}
